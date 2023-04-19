1f4834ebd148aaa00376344361b91aacc7c46ec7
JEOZKRAKEN <122051849+JEOZBOT@users.noreply.github.com>
2023-04-19 09:40:24 -0600

    map

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,14 @@ import Footer from "../components/Footer";
 import HeroImage from "../components/HeroImage";
 import Concept from "../components/Concept";
 import Gallery from "../components/Gallery";
-import VirtualTour from "../components/VirtualTour";
+
 import MasterPlan from "../components/MasterPlan";
 import Banner from "../components/Banner";
 import FullWidthImage from "../components/FullWidthImage";
 import Houses from "../components/Houses";
 import TextBanner from "../components/TextBanner";
 import MapSection from "../components/MapSection";
-import ProgressSlider from "../components/ProgressSlider";
+
 import Form from "../components/Form";
 
 const IndexPage = () => {
@@ -27,14 +27,14 @@ const IndexPage = () => {
       <HeroImage setOpenedForm={setOpenedForm} />
       <Concept />
       <Gallery />
-      <VirtualTour />
+
       <MasterPlan />
       <Banner setOpenedForm={setOpenedForm} />
       <FullWidthImage />
       <Houses setOpenedForm={setOpenedForm} />
       <TextBanner />
       <MapSection />
-      <ProgressSlider />
+
       <Footer setOpenedForm={setOpenedForm} />
       <Form opened={openedForm} setOpenedForm={setOpenedForm} />
     </Layout>
